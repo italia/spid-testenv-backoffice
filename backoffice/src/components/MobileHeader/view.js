34f283cd734f087d1441e6ae01006a9472c0bce1
damikael <michele.damico@linfaservice.it>
2017-10-05 18:40:20 +0200

    version number on header

--- backoffice/src/components/MobileHeader/view.js
+++ backoffice/src/components/MobileHeader/view.js
@@ -5,7 +5,7 @@ function view(me) {
     return (
        
             <div className="app-header app-header-design-blue navbar">
-             <a href="/" className="navbar-brand">  </a>
+             <a href="/" className="navbar-brand">  <span className="titleTag">TestEnv</span><span className="betaTag"> v0.9-beta.1</span> </a> 
                 <ul className="nav navbar-nav ml-auto">
                     <li>
                         <a href="/" 
