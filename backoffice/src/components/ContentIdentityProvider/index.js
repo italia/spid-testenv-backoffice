20de64ae9ae863efd6e191681b69220d14eb7f25
damikael <michele.damico@linfaservice.it>
2017-10-07 18:37:44 +0200

    IDP xml

--- backoffice/src/components/ContentIdentityProvider/index.js
+++ backoffice/src/components/ContentIdentityProvider/index.js
@@ -9,7 +9,61 @@ import Actions_Util from "../../redux/Util/Actions";
 class ContentIdentityProvider extends Component {
 
 	utilStore = ReduxStore.getUtil();
-	state = {};
+	state = {
+		xml: " \n\n\
+		<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n\
+		<EntityDescriptor xmlns=\"urn:oasis:names:tc:SAML:2.0:metadata\" ID=\"_2487e8a5-3a05-488c-8fa6-fe32b8ae96c8\" entityID=\"<entityID>\">  \n\
+		  <!-- Per test non viene richiesta la firma del metadata come avverrebbe in produzione --> \n\
+		  <IDPSSODescriptor WantAuthnRequestsSigned=\"true\" protocolSupportEnumeration=\"urn:oasis:names:tc:SAML:2.0:protocol\"> \n\
+			<KeyDescriptor use=\"signing\"> \n\
+			  <KeyInfo xmlns=\"http://www.w3.org/2000/09/xmldsig#\"> \n\
+				<X509Data> \n\
+				  <!-- certificato di default dell'identity server, se modificato è necessario cambiarlo --> \n\
+				  <X509Certificate> \n\
+					MIICNTCCAZ6gAwIBAgIES343gjANBgkqhkiG9w0BAQUFADBVMQswCQYDVQQGEwJVUzELMAkGA1UE \n\
+					CAwCQ0ExFjAUBgNVBAcMDU1vdW50YWluIFZpZXcxDTALBgNVBAoMBFdTTzIxEjAQBgNVBAMMCWxv \n\
+					Y2FsaG9zdDAeFw0xMDAyMTkwNzAyMjZaFw0zNTAyMTMwNzAyMjZaMFUxCzAJBgNVBAYTAlVTMQsw \n\
+					CQYDVQQIDAJDQTEWMBQGA1UEBwwNTW91bnRhaW4gVmlldzENMAsGA1UECgwEV1NPMjESMBAGA1UE \n\
+					AwwJbG9jYWxob3N0MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCUp/oV1vWc8/TkQSiAvTou \n\
+					sMzOM4asB2iltr2QKozni5aVFu818MpOLZIr8LMnTzWllJvvaA5RAAdpbECb+48FjbBe0hseUdN5 \n\
+					HpwvnH/DW8ZccGvk53I6Orq7hLCv1ZHtuOCokghz/ATrhyPq+QktMfXnRS4HrKGJTzxaCcU7OQID \n\
+					AQABoxIwEDAOBgNVHQ8BAf8EBAMCBPAwDQYJKoZIhvcNAQEFBQADgYEAW5wPR7cr1LAdq+IrR44i \n\
+					QlRG5ITCZXY9hI0PygLP2rHANh+PYfTmxbuOnykNGyhM6FjFLbW2uZHQTY1jMrPprjOrmyK5sjJR \n\
+					O4d1DeGHT/YnIjs9JogRKv4XHECwLtIVdAbIdWHEtVZJyMSktcyysFcvuhPQK8Qc/E/Wq8uHSCo= \n\
+					</X509Certificate> \n\
+				</X509Data> \n\
+			  </KeyInfo> \n\
+			</KeyDescriptor> \n\
+			<SingleLogoutService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect\" Location=\"https://<host_spid_idptest>:<port_spid_idptest>/samlsso\" ResponseLocation=\"https://<host_spid_idptest>:<port_spid_idptest>/samlsso/\"> \n\
+			<NameIDFormat>urn:oasis:names:tc:SAML:2.0:nameid-format:transient</NameIDFormat> \n\
+			<SingleSignOnService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\" Location=\"https://<host_spid_idptest>:<port_spid_idptest>/samlsso\"/> \n\
+			<SingleSignOnService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect\" Location=\"<host_spid_idptest>:<port_spid_idptest>/samlsso\"/> \n\
+			<saml2:Attribute Name=\"spidCode\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"name\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"familyName\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"placeOfBirth\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"countyOfBirth\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"dateOfBirth\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"gender\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"companyName\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"registeredOffice\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"fiscalNumber\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"ivaCode\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"idCard\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"mobilePhone\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"email\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"address\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"expirationDate\" NameFormat=\"xsi:string\"/> \n\
+			<saml2:Attribute Name=\"digitalAddress\" NameFormat=\"xsi:string\"/> \n\
+		  </IDPSSODescriptor> \n\
+		  <Organization> \n\
+			<OrganizationName xml:lang=\"it\">SPID Test Environment</OrganizationName> \n\
+			<OrganizationDisplayName xml:lang=\"it\">SPID TestEnvironment</OrganizationDisplayName> \n\
+			<OrganizationURL xml:lang=\"it\">https://www.spid.gov.it</OrganizationURL> \n\
+		  </Organization> \n\
+		</EntityDescriptor> \n\n\
+		"
+	};
 
 	constructor(props) {
 		super(props);
--- backoffice/src/components/ContentIdentityProvider/view.js
+++ backoffice/src/components/ContentIdentityProvider/view.js
@@ -1,45 +1,24 @@
 import React from 'react';
 import BlockUi from 'react-block-ui';
 import 'react-block-ui/style.css';
+import AceEditor from 'react-ace';
 import './style.css';
 
 function view(me) { 
     return(
 
-		<div className="container container-boxed">
-			<p>
-				<b>Entity ID</b><hr/>
-				&lt;entityID&gt;
-	
-				<br/>
-			</p>	
-			<p>
-				<b>AssertionConsumerService</b><hr/>
-				https://&lt;host_spid_idptest&gt;:&lt;port_spid_idptest&gt;/samlsso
-	
-				<br/>
-			</p>
-			<p>
-				<b>SingleLogoutService</b><hr/>
-				https://&lt;host_spid_idptest&gt;:&lt;port_spid_idptest&gt;/samlsso
-	
-				<br/>
-			</p>				
-	
-			<p>
-				<b>X509 Certificate</b><hr/>
-				MIICNTCCAZ6gAwIBAgIES343gjANBgkqhkiG9w0BAQUFADBVMQswCQYDVQQGEwJVUzELMAkGA1UE<br/>
-				CAwCQ0ExFjAUBgNVBAcMDU1vdW50YWluIFZpZXcxDTALBgNVBAoMBFdTTzIxEjAQBgNVBAMMCWxv<br/>
-				Y2FsaG9zdDAeFw0xMDAyMTkwNzAyMjZaFw0zNTAyMTMwNzAyMjZaMFUxCzAJBgNVBAYTAlVTMQsw<br/>
-				CQYDVQQIDAJDQTEWMBQGA1UEBwwNTW91bnRhaW4gVmlldzENMAsGA1UECgwEV1NPMjESMBAGA1UE<br/>
-				AwwJbG9jYWxob3N0MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCUp/oV1vWc8/TkQSiAvTou<br/>
-				sMzOM4asB2iltr2QKozni5aVFu818MpOLZIr8LMnTzWllJvvaA5RAAdpbECb+48FjbBe0hseUdN5<br/>
-				HpwvnH/DW8ZccGvk53I6Orq7hLCv1ZHtuOCokghz/ATrhyPq+QktMfXnRS4HrKGJTzxaCcU7OQID<br/>
-				AQABoxIwEDAOBgNVHQ8BAf8EBAMCBPAwDQYJKoZIhvcNAQEFBQADgYEAW5wPR7cr1LAdq+IrR44i<br/>
-				QlRG5ITCZXY9hI0PygLP2rHANh+PYfTmxbuOnykNGyhM6FjFLbW2uZHQTY1jMrPprjOrmyK5sjJR<br/>
-				O4d1DeGHT/YnIjs9JogRKv4XHECwLtIVdAbIdWHEtVZJyMSktcyysFcvuhPQK8Qc/E/Wq8uHSCo=<br/>
-			</p>		
-		</div>
+        <AceEditor
+            mode="xml"
+            theme="cobalt"
+            name="AceEditor"
+            className="AceEditor"
+            defaultValue={me.state.xml}
+            value={me.state.xml}
+            readOnly={true}
+            maxLines={10000}
+            editorProps={{$blockScrolling: true}}
+        />
+
     );
 }
 
